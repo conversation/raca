4886644463f583594fe64b289fd5ba97dcd0b387
James Healy <james@yob.id.au>
2014-04-09 23:37:17 +1000

    convert webmock specs for Raca::Server into standard mocks
    * Raca::Server now interacts with a Raca::HttpClient instance. How
      the HttpClient decides to send the message over the wire is an
      implementation detail so we don't want to test it. We can just mock
      out HttpClient and ensure it receives the correct messages

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -7,46 +7,9 @@ describe Raca::Server do
     info.stub(:public_endpoint).with("cloudServersOpenStack", :ord).and_return("https://the-cloud.com/account")
     info.stub(:auth_token).and_return('token')
     info.stub(:refresh_cache).and_return(true)
-    info.stub(:http_client).and_return(Raca::HttpClient.new(info, "the-cloud.com"))
     info
   }
-
-  before(:each) do
-
-    stub_request(:get, "https://the-cloud.com/account/servers/123").with(
-      :headers => {
-        'Accept'=>'application/json',
-        'Content-Type'=>'application/json',
-        'X-Auth-Token'=>'token'
-      }
-    ).to_return(
-      :status => 200,
-      :body => JSON.dump(
-        {server:
-          {progress:100,
-           id:123,
-           imageId:112,
-           flavorId:2,
-           status:"ACTIVE",
-           name:"server1",
-           hostId:"a42e65b66e4d7fa76c0ccdd01de25cb3",
-           addresses: {
-            public:["50.56.202.20"],
-            private:["10.178.237.99"]
-          },
-          metadata:{}}
-        }
-      )
-    )
-
-    stub_request(:delete, "https://the-cloud.com/account/servers/123").with(
-      :headers => {
-        'Accept'=>'application/json',
-        'Content-Type'=>'application/json',
-        'X-Auth-Token'=>'token'
-      }
-    ).to_return(:status => 204, :body => '')
-  end
+  let!(:http_client) { Raca::HttpClient.new(account, "the-cloud.com") }
 
   describe '#initialization' do
 
@@ -63,18 +26,52 @@ describe Raca::Server do
   end
 
   describe '#details' do
+    before(:each) do
+      account.stub(:http_client).and_return(http_client)
+
+      http_client.should_receive(:get).with("/account/servers/123", 'Accept'=>'application/json', 'Content-Type'=>'application/json').and_return(
+      double("Net::HTTPSuccess",
+              body: JSON.dump(
+                {server:
+                 {progress:100,
+                  id:123,
+                  imageId:112,
+                  flavorId:2,
+                  status:"ACTIVE",
+                  name:"server1",
+                  hostId:"a42e65b66e4d7fa76c0ccdd01de25cb3",
+                  addresses: {
+                    public:["50.56.202.20"],
+                    private:["10.178.237.99"]
+                  },
+                  metadata:{}}
+                }
+              )
+          )
+      )
+    end
 
     context "with an existing server" do
       let!(:server) { Raca::Server.new(account, :ord, "123") }
 
       it 'should return a hash of interesting data' do
-        server.details.should be_a(Hash)
-        server.details["status"].should == "ACTIVE"
+        server.details.tap { |result|
+          result.should be_a(Hash)
+          result["status"].should == "ACTIVE"
+        }
       end
     end
   end
 
   describe '#delete!' do
+
+    before(:each) do
+      account.stub(:http_client).and_return(http_client)
+
+      http_client.should_receive(:delete).with("/account/servers/123", 'Accept'=>'application/json', 'Content-Type'=>'application/json').and_return(
+        Net::HTTPNoContent.new("1.1", "204", "OK")
+      )
+    end
 
     context "with an existing server" do
       let!(:server) { Raca::Server.new(account, :ord, "123") }
